16a04375895a8d03784f41d1c5e8a4d2083c00a3
GianellaAnchivilca19 <142622889+GianellaAnchivilca19@users.noreply.github.com>
2024-04-27 14:02:33 -0500

    Add files via upload

---
+++ src/sesion5/Sesion5.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sesion5;
+
+import sesion5.pantallas.PantallaPersonas;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Sesion5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Crea el objeto de JFrame
+        PantallaPersonas pantalla = new PantallaPersonas();
+        //Muestra ek JFrame 
+        pantalla.setVisible(true);
+        //Centra el JFrame en la pantalla
+        pantalla.setLocationRelativeTo(null);
+    }
+    
+}
